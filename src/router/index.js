76b39868f88f885283e0009052dea12e3761d661
ph1234-Hawk <35072905+ph1234-dev@users.noreply.github.com>
2023-03-07 01:39:32 +0800

    updated routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ChatView from '../views/ChatView.vue'
 import RegisterView from '../views/RegisterView.vue'
+import SuccessfulRegisterView from "../views/SuccessRegisterView.vue"
+import SignInView from "../views/SignInView.vue"
+import PrivacyView from "../views/PrivacyView.vue"
+import AuthorView from "../views/AuthorView.vue"
+import AboutView from "../views/AboutView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,10 +16,36 @@ const router = createRouter({
       component: ChatView
     },
     {
+      path: '/privacy',
+      name: 'privacy_statement',
+      component: PrivacyView
+    },
+    {
+      path: '/about',
+      name: 'about_statement',
+      component: AboutView
+    },
+    {
+      path: '/author',
+      name: 'author_statement',
+      component: AuthorView
+    },
+    {
       path: '/register',
       name: 'register',
-      component: RegisterView
+      component: RegisterView,
     },
+    {
+      path: '/login',
+      name: 'login',
+      component: SignInView,
+    },
+    {
+      // you need to add router guard here
+      path: '/register/successful',
+      name: 'successful_register',
+      component: SuccessfulRegisterView
+    }
   ]
 })
 
