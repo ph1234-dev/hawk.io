f4757c529cbe51e8a1b1456b151361de511a57aa
ph1234-Hawk <35072905+ph1234-dev@users.noreply.github.com>
2023-03-13 12:36:47 +0800

    updated how backend is accessed by the api

--- src/stores/api.js
+++ src/stores/api.js
@@ -9,10 +9,12 @@ export const useBackendAPI = defineStore('backend', () => {
   let userInitialValue = {
     id: '',
     name: '',
-    authenticated: false
+    authenticated: false,
+    token: ''
   }
   let user = reactive(userInitialValue)
 
+  // const URL_BASE = "http://flask.activlab.pro/"
   const URL_BASE = "http://127.0.0.1:5000"
   const URL_REGISTER = `${URL_BASE}/user/api/register`
   const URL_LOGIN = `${URL_BASE}/user/api/login`
@@ -28,9 +30,9 @@ export const useBackendAPI = defineStore('backend', () => {
 
   // https://www.youtube.com/watch?v=059fh7Gobho
   // persisting vue pinia using watch
-  watch(user,userVal=>{
-    localStorage.setItem("user",JSON.stringify(userVal))
-  },{deep:true})
+  // watch(user,userVal=>{
+  //   localStorage.setItem("user",JSON.stringify(userVal))
+  // },{deep:true})
   // // adding deep is true here meaning we will track depe changes
 
   let router = useRouter()
@@ -39,7 +41,6 @@ export const useBackendAPI = defineStore('backend', () => {
     user.id = ''
     user.name = ''
     user.authenticated = false
-
       router.push({
         name: 'home'
     })
@@ -57,7 +58,7 @@ export const useBackendAPI = defineStore('backend', () => {
     // user.name =responseData
 
     let status = true
-    await requestData(URL_LOGIN, data, 'post')
+    await requestData(URL_LOGIN, data, 'POST')
       .then(data=>{
         if ( data.status != 200){
           console.error('erorr happend')
@@ -70,6 +71,8 @@ export const useBackendAPI = defineStore('backend', () => {
           user.id = responseData.id
           user.name = responseData.name
           user.authenticated = true
+          user.token = responseData.token
+          // console.log(`Received User Token: ${user.token}`)
       }).catch(e=>{
         console.log(`Call to API_ENDPOINT failed`, e)
         status = false
@@ -78,41 +81,54 @@ export const useBackendAPI = defineStore('backend', () => {
       return status
   }
 
-  let registerUser = (username, password, name) => {
+  let registerUser = async (username, password, name) => {
     // console.log(`Unimplemented [registerUser] - ${username} ${password} ${name}`)
     let data = {
       "username": username,
       "password": password,
-      "name": name
+      "name": name,
     }
 
-    requestData(URL_REGISTER, data, 'POST')
+    let status = await requestData(URL_REGISTER, data, 'POST')
       .then((data) => {
         return data.json()
       }).then((data)=>{
         user.id =data.id
         user.name =data.name
         user.authenticated = true
+        user.token = data.token
         console.log(data)
+        
+        //meaning query was accepted 
+        return true
       }).catch(e=>{
         console.error("Could not register user data, ",e)
+        // return false means there was an error 
+        return false
       })
+      // console.log(`Status from api: ${status}`)
+    return status
   }
 
-  let storeLog = (userID, user, bot) => {
-    // console.log("callings store log")
-    // 
+  let storeLog = (userMessage, botReply, lang="not specified due to test") => {
+    
     let data = {
-      user_id: userID,
-      message: user,
-      reply: bot
+      user_id: user.authenticated ? user.id: 6,
+      message: userMessage,
+      reply: botReply,
+      predicted_message_language: lang
     }
 
-    requestData(URL_STORE_LOG, data, 'POST')
+
+    requestData(URL_STORE_LOG, data, 'POST', user.token)
       .then((data) => {
         console.log(data); // JSON data parsed by `data.json()` call
+      })
+      .catch(e => {
+        console.error('Action: Attempted to store log into server. It was Executed but error was found. ' ,e)
       });
 
+    
     // Read This
     // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
     // Example POST method implementation:
@@ -125,22 +141,22 @@ export const useBackendAPI = defineStore('backend', () => {
     status = false
     if (status) {
       //connect to backend            
-      let data = { "message": msg}
+      // let data = { "message": msg}
 
-      https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Fetching_data
-      await requestData(URL_PREDICT_LANGUAGE,data,'POST')
-        .then((response)=>{
-          // first then returns a promise
-          if (!response.ok) {
-            throw new Error(`HTTP error: ${response.status}`);
-          }
-          // return here is the value of the resolve
-          // this returns a promise to lets resolve it and chain 
-          // its value on the next then just like a promise
-          return response.json();
-        }).then((data)=>{
-          lang = data.lang
-        }).catch((err) => console.error(`Fetch problem: ${err.message}`));
+      // https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Fetching_data
+      // await requestData(URL_PREDICT_LANGUAGE,data,'POST',user.token)
+      //   .then((response)=>{
+      //     // first then returns a promise
+      //     if (!response.ok) {
+      //       throw new Error(`HTTP error: ${response.status}`);
+      //     }
+      //     // return here is the value of the resolve
+      //     // this returns a promise to lets resolve it and chain 
+      //     // its value on the next then just like a promise
+      //     return response.json();
+      //   }).then((data)=>{
+      //     lang = data.lang
+      //   }).catch((err) => console.error(`Fetch problem: ${err.message}`));
       
     } else {
       // else use engine predict
@@ -154,22 +170,41 @@ export const useBackendAPI = defineStore('backend', () => {
   let getReply = async (msg) => {
     let reply = "response wasn't found"
     
-    let identifyLanguage = new Promise((resolve, reject) => {
+    // step 1 identify language
+    let identifyLanguage = new Promise((resolve,reject)=>{
       let lang = getLanguage(msg)
+      console.log('Promise Lang: ', lang)
       resolve(lang)
-    }).then(response => {
-      let lang = response
-      console.log(`Attempting to predict lang using: `, lang)
-      reply = ENGINE.getReply(msg,lang)
-      console.log(`Response from getReply[API] => ${reply}`)
+    }).then((response)=>{
+      return response
     }).catch(e=>{
-      console.log(`err from getReply[API] => ${e}`)
+      console.error(`Something went wrong. Error detected in classifying langauge. `,e)
+      return 'unspecified due to errors'
     })
 
-    await identifyLanguage
+    let lang = await identifyLanguage
+    console.log(`Test:\n\tUser message: ${msg}\n\tPredicted Language: ${lang}`)
+ 
+    // step 2 retrieve 
 
-    console.log(`Getting reply for ${msg}\n\tResponse: ${reply}`)
-    return reply
+    let identifyResponse = new Promise((resolve, reject) => {
+      reply = ENGINE.getReply(msg,lang)
+      resolve(reply)
+    }).then(response => {
+      console.log(`Retrieving reponse for msg: ${msg}`)
+      console.log(`Target language rules: ${lang}`)
+      return response
+    }).catch(e=>{
+      console.log(`Something went wrong. Error detected in getting reply, ${e}`)
+    })
+
+    reply = await identifyResponse
+
+    // console.log(`Getting reply for ${msg}\n\tResponse: ${reply}`)
+    return {
+      lang,
+      reply
+    }
   }
 
   return {
@@ -184,12 +219,12 @@ export const useBackendAPI = defineStore('backend', () => {
 
 // CHECKS IF APP IS ONLINE OR OFFLINE
 // https://stackoverflow.com/questions/189430/detect-the-internet-connection-is-offline
-console.log('Initially ' + (window.navigator.onLine ? 'on' : 'off') + 'line ' + window.navigator.onLine);
+// console.log('Initially ' + (window.navigator.onLine ? 'on' : 'off') + 'line ' + window.navigator.onLine);
 
 window.addEventListener('online', () => console.log('Became online ', window.navigator.onLine));
 window.addEventListener('offline', () => console.log('Became offline'));
 
-async function requestData(url = '', data = {}, operation = 'GET') {
+async function requestData(url = '', data = {}, operation = 'GET',token='') {
   // console.log(`JSON passed: ${data}`)
 
   // Default options are marked with *
@@ -199,7 +234,8 @@ async function requestData(url = '', data = {}, operation = 'GET') {
     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
     credentials: 'same-origin', // include, *same-origin, omit
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
       // 'Content-Type': 'application/x-www-form-urlencoded',
     },
     redirect: 'follow', // manual, *follow, error
