3a9a07a24c0d1c562382e6bc906934bd93306544
ph1234-Hawk <35072905+ph1234-dev@users.noreply.github.com>
2023-03-23 19:49:37 +0800

    added classifier class for identifying selfcare actions

---
+++ src/api/domain-classifier.js
@@ -0,0 +1,92 @@
+import NaiveBayes from "./classifier/naive-bayes"
+
+
+class AnnotatedDocument {
+
+    constructor(data,lang){
+        this.data = data
+        this.class = lang
+    }
+}
+
+export default class DomainClassifier{
+
+    constructor(){
+
+        this.nb = new NaiveBayes()
+
+        this.classes = []
+
+        // annotated
+        this.documents = []
+
+    }
+
+    insertCluster(rules,lang){
+
+        let insertCallback = (doc)=>{
+
+            // data clearning 
+            // step1 replace non-word with space
+            doc = doc.replace(/\W/g, " ").replace(/\s\s+/g, ' ')
+            
+            // step2 lowercase to reduce reduncant
+            doc = doc.toLowerCase()
+
+            // step3 is to ignore the
+
+
+            let data = new AnnotatedDocument(
+                doc,
+                lang
+            )
+
+            this.documents.push(data)
+            
+            // train only works if its array
+            // so lets twap this with array
+            this.nb.train([data])
+
+        }
+
+        rules.forEach((rule, index) => {
+            // access patters
+            let patterns = rule.pattern
+            patterns.forEach(insertCallback)
+        })
+
+    }
+
+    buildTermProbabilityMap(){
+        this.nb.buildTermProbabilityMap()
+    }
+
+    getPrediction(msg){
+        // find max
+        return this.nb.predict(msg)
+    }
+
+
+    printDocuments(){
+        let size = this.documents.length
+        console.log(`Printing Documents (size: ${size})`)
+        // this.documents.forEach((docs,index)=>{
+        //     console.log(`${index},${docs.class},${docs.data}`)
+        //     // console.log(`${JSON.stringify(docs)}`)
+        // })
+        
+        let strrep = ''
+        this.documents.forEach((docs,index)=>{
+            let d = `${index},${docs.class},${docs.data}`
+            // corpus.push(d)
+            strrep += `${d}\n`
+        })
+        console.log(strrep)
+    }
+
+    printClassifierPropertyValues(){
+        console.log("Attemptin to print classifier properties")
+        this.nb.print().vocabulary()
+        this.nb.print().class_terms()
+    }
+}
